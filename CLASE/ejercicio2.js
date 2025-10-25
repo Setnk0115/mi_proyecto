5ba0cf00a33420a54f6509c38b12241b235db13e
Sebastian_Ramirez_Castrillon <sebas.ramirez.0115@gmail.com>
2025-10-25 18:57:26 -0500

    feat: agregar ejercicio 2

---
+++ CLASE/ejercicio2.js
@@ -0,0 +1,14 @@
+
+let nombre = "Sebastian Ramirez Castrillon";
+let materia1 = 4.0;
+let materia2 = 3.0;
+let materia3 = 4.5;
+
+let promedio = (materia1 + materia2 + materia3) / 3;
+
+if (promedio >= 3.5 ) {
+    console.log(`El estudiante ${nombre} aprobó con un promedio de: ${promedio}`);
+    
+} else{
+    console.log(`El estudiante ${nombre} no aprobó con un promedio de: ${promedio}`)
+}
