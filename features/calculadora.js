48a0c28e32e495d62095ea2bf82002b911e6ba61
Sebastian_Ramirez_Castrillon <sebas.ramirez.0115@gmail.com>
2025-11-09 18:57:38 -0500

    chore: Removiendo carpetas feature y utils que fueron duplicadas por error

--- features/calculadora.js
+++
@@ -1,8 +0,0 @@
-const readlineSync = require("readline-sync");
-
-const numero1 = readlineSync.questionFloat("Digite el primer numero: ");
-const numero2 = readlineSync.questionFloat("Digite el segundo numero: ");
-
-const resultado = numero1 + numero2;
-
-console.log(`La suma de ${numero1} + ${numero2} es = ${resultado}`);
